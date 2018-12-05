105b6804e7da8dbd84d41d71c1f27b0890a8ccb6
Hong jinho <tpdleps@gmail.com>
2018-12-05 11:11:43 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,2 +1,21 @@
 resource "null_resource" "example" {
 }
+resource "aws_dynamodb_table" "basic-dynamodb-table" {
+  name           = "GameScores"
+  read_capacity  = 1
+  write_capacity = 1
+  hash_key       = "UserId"
+  range_key      = "GameTitle"
+
+  attribute {
+    name = "UserId"
+    type = "S"
+  }
+
+  attribute {
+    name = "GameTitle"
+    type = "S"
+  }
+
+ 
+}
